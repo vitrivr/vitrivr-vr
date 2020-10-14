9df3bb9956990a56eeb66ff546911f4a78b730c3
Florian Spiess <florianms@icloud.com>
2020-10-14 17:07:44 +0200

    Added first versions of follow behavior scripts.

---
+++ Assets/Scripts/VitrivrVR/Behavior/FollowPositionController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace VitrivrVR.Behavior
+{
+  public class FollowPositionController : MonoBehaviour
+  {
+    public bool teleportOnEnable = true;
+    public Transform target;
+    public float maxSpeed = 2;
+
+    private void OnEnable()
+    {
+      if (teleportOnEnable)
+      {
+        transform.position = target.position;
+      }
+    }
+
+    private void Update()
+    {
+      var tfm = transform;
+      tfm.position = Vector3.MoveTowards(tfm.position, target.position, maxSpeed * Time.deltaTime);
+    }
+  }
+}
---
+++ Assets/Scripts/VitrivrVR/Behavior/FollowRotationController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace VitrivrVR.Behavior
+{
+  public class FollowRotationController : MonoBehaviour
+  {
+    public bool overwriteOnEnable = true;
+    public Transform target;
+    public float maxAngularSpeed = 360;
+
+    private void OnEnable()
+    {
+      if (overwriteOnEnable)
+      {
+        transform.rotation = target.rotation;
+      }
+    }
+
+    private void Update()
+    {
+      var tfm = transform;
+      tfm.rotation = Quaternion.RotateTowards(tfm.rotation, target.rotation, maxAngularSpeed * Time.deltaTime);
+    }
+  }
+}
