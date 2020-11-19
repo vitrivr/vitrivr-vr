a9a46567273f044e3ffb9dda3bf8b1655812ff2a
Florian Spiess <florian.spiess@unibas.ch>
2020-11-19 11:41:48 +0100

    Implemented basic UI tables for easy data viewing.

---
+++ Assets/Scripts/VitrivrVR/Data/UITableController.cs
@@ -0,0 +1,47 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace VitrivrVR.Data
+{
+  public class UITableController : MonoBehaviour
+  {
+    public string[,] table;
+    public float minimumColumnWidth = 50;
+    public float columnPadding = 20;
+    public GameObject cellPrefab;
+
+    private void Start()
+    {
+      if (table != null)
+      {
+        Initialize();
+      }
+    }
+
+    public void Initialize()
+    {
+      var columnGameObject = new GameObject("Column", typeof(VerticalLayoutGroup));
+      var layoutGroup = columnGameObject.GetComponent<VerticalLayoutGroup>();
+      layoutGroup.childControlHeight = false;
+
+      var cellHeight = cellPrefab.GetComponent<RectTransform>().sizeDelta.y;
+
+      for (var i = 0; i < table.GetLength(0); i++)
+      {
+        var column = Instantiate(columnGameObject, transform);
+        var columnWidth = minimumColumnWidth;
+        for (var j = 0; j < table.GetLength(1); j++)
+        {
+          var cell = Instantiate(cellPrefab, column.transform);
+          var tmp = cell.GetComponentInChildren<TextMeshProUGUI>();
+          tmp.text = table[i, j];
+          columnWidth = Mathf.Max(columnWidth, tmp.GetPreferredValues().x + columnPadding);
+        }
+
+        var rectTransform = column.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(columnWidth, table.GetLength(1) * cellHeight);
+      }
+    }
+  }
+}
