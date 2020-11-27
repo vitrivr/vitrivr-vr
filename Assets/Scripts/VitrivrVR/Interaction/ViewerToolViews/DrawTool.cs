d33668488975f973120d0df22e71700a16ee6b09
Florian Spiess <florian.spiess@unibas.ch>
2020-11-27 11:49:38 +0100

    Updated draw tool to support multisegment drawing.

--- Assets/Scripts/VitrivrVR/Interaction/ViewerToolViews/DrawTool.cs
+++ Assets/Scripts/VitrivrVR/Interaction/ViewerToolViews/DrawTool.cs
@@ -7,19 +7,23 @@ namespace VitrivrVR.Interaction.ViewerToolViews
   {
     public Material lineMaterial;
     public float lineWidth = 0.01f;
-    
+    public float maxSegmentDistance = 0.02f;
+    public float minCornerAngle = 10;
+
     private XRButtonObserver _buttonObserver;
     private LineRenderer _currentLine;
     private LineRenderer _lineGameObject;
-    
+    private float _sqrMaxSegmentDistance;
+
     private void Awake()
     {
+      _sqrMaxSegmentDistance = maxSegmentDistance * maxSegmentDistance;
       _buttonObserver = FindObjectOfType<XRButtonObserver>();
       if (!_buttonObserver)
       {
         Debug.LogError("Could not find required XRButtonObserver in scene!");
       }
-      
+
       var go = new GameObject("DrawLine", typeof(LineRenderer));
       _lineGameObject = go.GetComponent<LineRenderer>();
       _lineGameObject.material = lineMaterial;
@@ -32,7 +36,35 @@ namespace VitrivrVR.Interaction.ViewerToolViews
     {
       if (_currentLine)
       {
-        _currentLine.SetPosition(1, transform.position);
+        // Update current position
+        var numPositions = _currentLine.positionCount;
+        var position = transform.position;
+        _currentLine.SetPosition(numPositions - 1, position);
+        // If the last position was far enough away, create new point
+        var lastPosition = _currentLine.GetPosition(numPositions - 2);
+        if ((lastPosition - position).sqrMagnitude > _sqrMaxSegmentDistance)
+        {
+          // Check if the last position can be removed
+          if (numPositions > 2)
+          {
+            var rootPosition = _currentLine.GetPosition(numPositions - 3);
+            var angle = Vector3.Angle(lastPosition - rootPosition, position - lastPosition);
+            if (angle < minCornerAngle)
+            {
+              _currentLine.SetPosition(numPositions - 2, position);
+            }
+            else
+            {
+              _currentLine.positionCount++;
+              _currentLine.SetPosition(numPositions, position);
+            }
+          }
+          else
+          {
+            _currentLine.positionCount++;
+            _currentLine.SetPosition(numPositions, position);
+          }
+        }
       }
     }
 
