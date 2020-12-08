360fc61b679ff21cf7ccaeec22d29e740494675f
Florian Spiess <florian.spiess@unibas.ch>
2020-12-08 17:51:48 +0100

    Started implementing pop-out video player.

--- Assets/Scripts/VitrivrVR/Media/CanvasMediaItemDisplay.cs
+++ Assets/Scripts/VitrivrVR/Media/CanvasMediaItemDisplay.cs
@@ -3,12 +3,10 @@ using System.Collections;
 using System.Threading.Tasks;
 using CineastUnityInterface.Runtime.Vitrivr.UnityInterface.CineastApi.Model.Data;
 using CineastUnityInterface.Runtime.Vitrivr.UnityInterface.CineastApi.Utils;
-using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Networking;
 using UnityEngine.UI;
-using UnityEngine.Video;
 using VitrivrVR.Config;
 using VitrivrVR.Util;
 
@@ -24,16 +22,14 @@ namespace VitrivrVR.Media
     public RawImage previewImage;
     public RawImage scoreFrame;
     public RectTransform imageFrame;
-    public TextMeshProUGUI segmentDataText;
     public int scoreFrameSize = 25;
-    public RectTransform progressBar;
-    public RectTransform progressIndicator;
-    public RectTransform segmentIndicator;
+
+    public CanvasVideoDisplay canvasVideoDisplay;
 
     private ScoredSegment _scoredSegment;
     private SegmentData _segment;
     private bool _videoInitialized;
-    private VideoPlayerController _videoPlayerController;
+    private CanvasVideoDisplay _videoDisplay;
 
     private void Awake()
     {
@@ -42,35 +38,12 @@ namespace VitrivrVR.Media
       clickHandler.onClick = OnClickImage;
     }
 
-    private void Start()
+    private void OnDestroy()
     {
-      var progressBarSizeDelta = progressBar.sizeDelta;
-      progressBarSizeDelta.y = scoreFrameSize;
-      progressBar.sizeDelta = progressBarSizeDelta;
-      var progressBarPos = progressBar.anchoredPosition;
-      progressBarPos.y = -scoreFrameSize / 2f;
-      progressBar.anchoredPosition = progressBarPos;
-    }
-
-    private void Update()
-    {
-      if (_videoInitialized && _videoPlayerController.IsPlaying)
+      if (_videoInitialized)
       {
-        UpdateProgressIndicator(_videoPlayerController.ClockTime);
+        Destroy(_videoDisplay.gameObject);
       }
-    }
-
-    private void UpdateProgressIndicator(double time)
-    {
-      progressIndicator.anchoredPosition =
-        new Vector2((float) (progressBar.rect.width * time / _videoPlayerController.Length), 0);
-    }
-
-    private void SetSegmentIndicator(double start, double end, double length)
-    {
-      var rect = progressBar.rect;
-      segmentIndicator.anchoredPosition = new Vector2((float) (rect.width * start / length), 0);
-      segmentIndicator.sizeDelta = new Vector2((float) (rect.width * (end - start) / length), 0);
     }
 
     public override ScoredSegment ScoredSegment => _scoredSegment;
@@ -86,7 +59,6 @@ namespace VitrivrVR.Media
       var config = CineastConfigManager.Instance.Config;
       var vrConfig = ConfigManager.Config;
       var score = (float) _scoredSegment.score;
-      segmentDataText.text = $"Segment {_segment.Id}\nScore: {score:F}";
       // Score frame
       scoreFrame.color = vrConfig.similarityColor.ToColor() * score +
                          vrConfig.dissimilarityColor.ToColor() * (1 - score);
@@ -111,85 +83,16 @@ namespace VitrivrVR.Media
     {
       if (_videoInitialized)
       {
-        if (_videoPlayerController.IsPlaying)
-        {
-          _videoPlayerController.Pause();
-        }
-        else
-        {
-          _videoPlayerController.Play();
-        }
+        Destroy(_videoDisplay.gameObject);
+        _videoInitialized = false;
       }
       else
       {
-        InitializeVideo();
+        _videoInitialized = true;
+        var t = transform;
+        _videoDisplay = Instantiate(canvasVideoDisplay, t.position - 0.2f * t.forward, t.rotation);
+        _videoDisplay.Initialize(_scoredSegment);
       }
-    }
-
-    private void OnClickProgressBar(PointerEventData pointerEventData)
-    {
-      var clickPosition = Quaternion.Inverse(Quaternion.LookRotation(progressBar.forward)) *
-                          (pointerEventData.pointerCurrentRaycast.worldPosition - progressBar.position);
-      var clickProgress = clickPosition.x + 0.5;
-      var newTime = _videoPlayerController.Length * clickProgress;
-      if (_videoPlayerController.IsPlaying)
-      {
-        _videoPlayerController.Pause();
-        _videoPlayerController.SetTime(newTime);
-        _videoPlayerController.Play();
-      }
-      else
-      {
-        _videoPlayerController.SetTime(newTime);
-      }
-
-      UpdateProgressIndicator(newTime);
-    }
-
-    /// <summary>
-    /// Initializes the <see cref="VideoPlayer"/> component of this display.
-    /// </summary>
-    private async void InitializeVideo()
-    {
-      // Check if segment has encountered error during initialization
-      if (!_segment.Initialized)
-      {
-        // Try again to initialize
-        try
-        {
-          await _segment.GetObjectId();
-        }
-        catch (Exception)
-        {
-          return;
-        }
-      }
-
-      // Set flag here to ensure video is only initialized once
-      _videoInitialized = true;
-
-      // Change texture to loading texture and reset scale
-      previewImage.texture = loadingTexture;
-      imageFrame.sizeDelta = new Vector2(1000, 1000);
-
-      // Resolve media URL
-      // TODO: Retrieve and / or apply all required media information, potentially from within PathResolver
-      var config = CineastConfigManager.Instance.Config;
-      var objectId = await _segment.GetObjectId();
-      var mediaPath = PathResolver.ResolvePath(config.mediaPath, objectId);
-      var mediaUrl = $"{config.mediaHost}{mediaPath}";
-
-      var startFrame = await _segment.GetStart();
-
-      _videoPlayerController =
-        new VideoPlayerController(gameObject, mediaUrl, startFrame, PrepareCompleted, ErrorEncountered);
-
-      var start = await _segment.GetAbsoluteStart();
-      var end = await _segment.GetAbsoluteEnd();
-      segmentDataText.text = $"Segment {_segment.Id}: {start:F}s - {end:F}s\nScore: {_scoredSegment.score:F}";
-
-      var progressClickHandler = progressBar.gameObject.AddComponent<ClickHandler>();
-      progressClickHandler.onClick = OnClickProgressBar;
     }
 
     /// <summary>
@@ -214,30 +117,6 @@ namespace VitrivrVR.Media
         imageFrame.sizeDelta =
           new Vector2(1000 * loadedTexture.width / factor, 1000 * loadedTexture.height / factor);
       }
-    }
-
-    private void PrepareCompleted(RenderTexture texture)
-    {
-      var width = _videoPlayerController.Width;
-      var height = _videoPlayerController.Height;
-      var factor = Mathf.Max(width, height);
-      previewImage.texture = texture;
-      imageFrame.sizeDelta = new Vector2(1000f * width / factor, 1000f * height / factor);
-
-      segmentDataText.rectTransform.anchoredPosition -= new Vector2(0, scoreFrameSize);
-
-      var start = _segment.GetAbsoluteStart().Result;
-      var end = _segment.GetAbsoluteEnd().Result;
-      var length = _videoPlayerController.Length;
-      UpdateProgressIndicator(start);
-      SetSegmentIndicator(start, end, length);
-      progressBar.gameObject.SetActive(true);
-    }
-
-    private void ErrorEncountered(VideoPlayer videoPlayer, string error)
-    {
-      Debug.LogError(error);
-      previewImage.texture = errorTexture;
     }
   }
 }
---
+++ Assets/Scripts/VitrivrVR/Media/CanvasVideoDisplay.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using CineastUnityInterface.Runtime.Vitrivr.UnityInterface.CineastApi.Model.Data;
+using CineastUnityInterface.Runtime.Vitrivr.UnityInterface.CineastApi.Utils;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using VitrivrVR.Util;
+
+namespace VitrivrVR.Media
+{
+  public class CanvasVideoDisplay : MonoBehaviour
+  {
+    public Texture2D errorTexture;
+    public Texture2D loadingTexture;
+    public RawImage previewImage;
+    public RectTransform progressBar;
+    public RectTransform progressIndicator;
+    public RectTransform segmentIndicator;
+    public TextMeshProUGUI segmentDataText;
+    public float progressBarSize = 100;
+
+    private ScoredSegment _scoredSegment;
+    private SegmentData _segment;
+    private VideoPlayerController _videoPlayerController;
+    private RectTransform _imageTransform;
+
+    public async void Initialize(ScoredSegment segment)
+    {
+      _scoredSegment = segment;
+      _segment = _scoredSegment.segment;
+      // Check if segment has encountered error during initialization
+      if (!_segment.Initialized)
+      {
+        // Try again to initialize
+        try
+        {
+          await _segment.GetObjectId();
+        }
+        catch (Exception)
+        {
+          return;
+        }
+      }
+
+      // Change texture to loading texture and reset scale
+      previewImage.texture = loadingTexture;
+      _imageTransform.sizeDelta = new Vector2(1000, 1000);
+
+      // Resolve media URL
+      // TODO: Retrieve and / or apply all required media information, potentially from within PathResolver
+      var config = CineastConfigManager.Instance.Config;
+      var objectId = await _segment.GetObjectId();
+      var mediaPath = PathResolver.ResolvePath(config.mediaPath, objectId);
+      var mediaUrl = $"{config.mediaHost}{mediaPath}";
+
+      var startFrame = await _segment.GetStart();
+
+      _videoPlayerController =
+        new VideoPlayerController(gameObject, mediaUrl, startFrame, PrepareCompleted, ErrorEncountered);
+
+      var start = await _segment.GetAbsoluteStart();
+      var end = await _segment.GetAbsoluteEnd();
+      segmentDataText.text = $"Segment {_segment.Id}: {start:F}s - {end:F}s\nScore: {_scoredSegment.score:F}";
+
+      var clickHandler = previewImage.gameObject.AddComponent<ClickHandler>();
+      clickHandler.onClick = OnClickImage;
+      var progressClickHandler = progressBar.gameObject.AddComponent<ClickHandler>();
+      progressClickHandler.onClick = OnClickProgressBar;
+    }
+
+    private void Awake()
+    {
+      GetComponent<Canvas>().worldCamera = Camera.main;
+      _imageTransform = previewImage.GetComponent<RectTransform>();
+    }
+    
+    private void Update()
+    {
+      if (_videoPlayerController != null && _videoPlayerController.IsPlaying)
+      {
+        UpdateProgressIndicator(_videoPlayerController.ClockTime);
+      }
+    }
+
+    private void OnClickImage(PointerEventData pointerEventData)
+    {
+      if (_videoPlayerController.IsPlaying)
+      {
+        _videoPlayerController.Pause();
+      }
+      else
+      {
+        _videoPlayerController.Play();
+      }
+    }
+    
+    private void OnClickProgressBar(PointerEventData pointerEventData)
+    {
+      var clickPosition = Quaternion.Inverse(Quaternion.LookRotation(progressBar.forward)) *
+                          (pointerEventData.pointerCurrentRaycast.worldPosition - progressBar.position);
+      var corners = new Vector3[4];
+      progressBar.GetWorldCorners(corners);
+      var progressBarWidth = (corners[1] - corners[2]).magnitude;
+
+      var clickProgress = clickPosition.x / progressBarWidth + 0.5;
+      var newTime = _videoPlayerController.Length * clickProgress;
+      if (_videoPlayerController.IsPlaying)
+      {
+        _videoPlayerController.Pause();
+        _videoPlayerController.SetTime(newTime);
+        _videoPlayerController.Play();
+      }
+      else
+      {
+        _videoPlayerController.SetTime(newTime);
+      }
+
+      UpdateProgressIndicator(newTime);
+    }
+
+    private void PrepareCompleted(RenderTexture texture)
+    {
+      var width = _videoPlayerController.Width;
+      var height = _videoPlayerController.Height;
+      var factor = Mathf.Max(width, height);
+      previewImage.texture = texture;
+      _imageTransform.sizeDelta = new Vector2(1000f * width / factor, 1000f * height / factor);
+      
+      segmentDataText.rectTransform.anchoredPosition -= new Vector2(0, progressBarSize);
+
+      var start = _segment.GetAbsoluteStart().Result;
+      var end = _segment.GetAbsoluteEnd().Result;
+      var length = _videoPlayerController.Length;
+      UpdateProgressIndicator(start);
+      SetSegmentIndicator(start, end, length);
+      // Set progress bar size
+      var progressBarSizeDelta = progressBar.sizeDelta;
+      progressBarSizeDelta.y = progressBarSize;
+      progressBar.sizeDelta = progressBarSizeDelta;
+      var progressBarPos = progressBar.anchoredPosition;
+      progressBarPos.y = -progressBarSize / 2f;
+      progressBar.anchoredPosition = progressBarPos;
+      progressBar.gameObject.SetActive(true);
+    }
+
+    private void ErrorEncountered(VideoPlayer videoPlayer, string error)
+    {
+      Debug.LogError(error);
+      previewImage.texture = errorTexture;
+    }
+    
+    private void UpdateProgressIndicator(double time)
+    {
+      progressIndicator.anchoredPosition =
+        new Vector2((float) (progressBar.rect.width * time / _videoPlayerController.Length), 0);
+    }
+
+    private void SetSegmentIndicator(double start, double end, double length)
+    {
+      var rect = progressBar.rect;
+      segmentIndicator.anchoredPosition = new Vector2((float) (rect.width * start / length), 0);
+      segmentIndicator.sizeDelta = new Vector2((float) (rect.width * (end - start) / length), 0);
+    }
+  }
+}
