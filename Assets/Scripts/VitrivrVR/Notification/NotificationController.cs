1c7881f80dc286a2a914675e76e503db29d67be2
Florian Spiess <florian.spiess@unibas.ch>
2021-01-13 17:29:02 +0100

    Implemented very basic Notification display system to display small debug messages in VR.

---
+++ Assets/Scripts/VitrivrVR/Notification/NotificationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace VitrivrVR.Notification
+{
+  public class NotificationController : MonoBehaviour
+  {
+    [Serializable]
+    public class NotificationEvent : UnityEvent<string>
+    {
+    }
+
+    private static NotificationController _instance;
+
+    public NotificationEvent notificationEvent;
+
+    private void Awake()
+    {
+      if (_instance != null)
+      {
+        Debug.LogError("NotificationController instance already registered! There are several " +
+                       "NotificationControllers in the scene!");
+      }
+      else
+      {
+        _instance = this;
+      }
+    }
+
+    public static void Notify(string notification)
+    {
+      if (_instance)
+      {
+        _instance.notificationEvent.Invoke(notification);
+      }
+      else
+      {
+        Debug.Log(notification);
+      }
+    }
+  }
+}
---
+++ Assets/Scripts/VitrivrVR/Notification/NotificationDisplay.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace VitrivrVR.Notification
+{
+  public class NotificationDisplay : MonoBehaviour
+  {
+    /// <summary>
+    /// Time until notification text disappears.
+    /// </summary>
+    public float fadeTime = 10;
+
+    private TextMeshPro _textDisplay;
+    private float _fadeTimer;
+
+    private void Start()
+    {
+      _textDisplay = GetComponent<TextMeshPro>();
+      NotificationController.Notify("Test Notification!");
+    }
+
+    private void Update()
+    {
+      if (!(_fadeTimer > 0)) return;
+      
+      _fadeTimer -= Time.deltaTime;
+      if (_fadeTimer <= 0)
+      {
+        _textDisplay.text = "";
+      }
+    }
+
+    public void SetNotification(string notification)
+    {
+      _textDisplay.text = notification;
+      _fadeTimer = fadeTime;
+    }
+  }
+}
