ca1c97f6822ae9be858bcbdbf7cec5f82395775f
Florian Spiess <florian.spiess@unibas.ch>
2021-01-14 12:08:14 +0100

    Updated CylinderQueryDisplay with performance improvements and polish.

--- Assets/Scripts/VitrivrVR/Query/Display/CylinderQueryDisplay.cs
+++ Assets/Scripts/VitrivrVR/Query/Display/CylinderQueryDisplay.cs
@@ -28,7 +28,10 @@ namespace VitrivrVR.Query.Display
     private int _nResults;
     private float _columnAngle;
     private int _maxColumns;
+
     private float _currentRotation;
+    private int _currentStart;
+    private int _currentEnd;
 
     private void Awake()
     {
@@ -46,22 +49,6 @@ namespace VitrivrVR.Query.Display
     private async void Update()
     {
       Rotate(Time.deltaTime * rotationSpeed * UnityEngine.Input.GetAxisRaw("Horizontal"));
-      
-      // Start includes items in the instantiation queue, since these will be instantiated shortly
-      // var start = _mediaDisplays.Count + _instantiationQueue.Count;
-      // var columns = start / rows;
-      //
-      // if (transform.position.x - rows * loadBuffer < -(1 + padding) * columns)
-      // {
-      //   var end = Mathf.Min((columns + 1) * rows, _nResults);
-      //   if (start < _nResults)
-      //   {
-      //     foreach (var segment in _results.GetRange(start, end - start))
-      //     {
-      //       _instantiationQueue.Enqueue(segment);
-      //     }
-      //   }
-      // }
 
       if (_instantiationQueue.Count > 0)
       {
@@ -88,11 +75,13 @@ namespace VitrivrVR.Query.Display
 
     private void Rotate(float degrees)
     {
-      _currentRotation -= degrees;
       transform.Rotate(degrees * Vector3.up);
-      
+      _currentRotation -= degrees;
+      // Subtract 90 from current rotation to have results replaced on the side and not in front of the user
+      var rawColumnIndex = Mathf.FloorToInt((_currentRotation - 90) / _columnAngle);
+
       // Check instantiations
-      var enabledEnd = Mathf.Min(Mathf.FloorToInt((_currentRotation + 360) / _columnAngle) * rows, _nResults);
+      var enabledEnd = Mathf.Min((rawColumnIndex + _maxColumns) * rows, _nResults);
       if (enabledEnd > _mediaDisplays.Count + _instantiationQueue.Count)
       {
         var start = _mediaDisplays.Count + _instantiationQueue.Count;
@@ -101,21 +90,29 @@ namespace VitrivrVR.Query.Display
           _instantiationQueue.Enqueue(segment);
         }
       }
-      
+
       // Check enabled
-      // TODO: Find better way to check only relevant displays rather than iterating over all displays every frame
-      var enabledStart = Math.Max(Mathf.FloorToInt(_currentRotation / _columnAngle) * rows, 0);
-      for (var i = 0; i < _mediaDisplays.Count; i++)
+      var enabledStart = Math.Max(rawColumnIndex * rows, 0);
+      if (enabledStart != _currentStart || enabledEnd != _currentEnd)
       {
-        _mediaDisplays[i].display.gameObject.SetActive(enabledStart <= i && i < enabledEnd);
+        var start = Mathf.Min(enabledStart, _currentStart);
+        var end = Mathf.Min(Mathf.Max(enabledEnd, _currentEnd), _mediaDisplays.Count);
+        for (var i = start; i < end; i++)
+        {
+          _mediaDisplays[i].display.gameObject.SetActive(enabledStart <= i && i < enabledEnd);
+        }
+
+        _currentStart = enabledStart;
+        _currentEnd = enabledEnd;
       }
     }
 
     private async Task CreateResultObject(ScoredSegment result)
     {
       // Determine position
-      var row = _mediaDisplays.Count % rows;
-      var column = _mediaDisplays.Count / rows;
+      var index = _mediaDisplays.Count;
+      var row = index % rows;
+      var column = index / rows;
       var multiplier = resultSize + padding;
       var position = new Vector3(0, multiplier * row, distance);
       var rotation = Quaternion.Euler(0, column * _columnAngle, 0);
@@ -128,9 +125,12 @@ namespace VitrivrVR.Query.Display
       // Adjust size
       transform2.localScale *= resultSize;
 
+
       _mediaDisplays.Add((itemDisplay, (float) result.score));
 
       await itemDisplay.Initialize(result);
+
+      itemDisplay.gameObject.SetActive(_currentStart <= index && index < _currentEnd);
     }
   }
 }
