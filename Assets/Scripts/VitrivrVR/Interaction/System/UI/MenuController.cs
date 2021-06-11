947c39733bca7419f16fef07637f13cf6ce71d37
Florian Spiess <florian.spiess@unibas.ch>
2021-06-11 16:31:49 +0200

    Removed physical menu toggle in favor of button press. Right primary button is now dictation and left primary button is menu toggle.

--- Assets/Scripts/VitrivrVR/Interaction/System/UI/MenuController.cs
+++ Assets/Scripts/VitrivrVR/Interaction/System/UI/MenuController.cs
@@ -1,20 +1,37 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace VitrivrVR.Interaction.System.UI
 {
   /// <summary>
   /// Basic interactable menu controller to allow toggling of menu items.
   /// </summary>
-  public class MenuController : Interactable
+  public class MenuController : MonoBehaviour
   {
+    public InputAction toggleAction;
+
     public List<Transform> menuItems;
 
     private bool _showing;
 
-    public override void OnInteraction(Transform _, bool start)
+    private void Awake()
     {
-      if (!start) return;
+      toggleAction.performed += ToggleMenu;
+    }
+
+    private void OnEnable()
+    {
+      toggleAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+      toggleAction.Disable();
+    }
+
+    private void ToggleMenu(InputAction.CallbackContext context)
+    {
       _showing = !_showing;
       foreach (var menuItem in menuItems)
       {
