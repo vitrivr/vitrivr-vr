cbb5cd825d293511599547acd84f047c8f145f53
Florian Spiess <florian.spiess@unibas.ch>
2021-10-27 14:24:00 +0200

    Implemented first version of MapTermProvider for providing map based spatial query terms.

---
+++ Assets/Scripts/VitrivrVR/Query/Term/MapTermProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Org.Vitrivr.CineastApi.Model;
+using Vitrivr.UnityInterface.CineastApi.Utils;
+
+namespace VitrivrVR.Query.Term
+{
+  /// <summary>
+  /// <see cref="QueryTermProvider"/> for spatial <see cref="QueryTerm"/>s through use of a <see cref="Map"/>.
+  /// </summary>
+  public class MapTermProvider : QueryTermProvider
+  {
+    public Map.Map map;
+
+    public override List<QueryTerm> GetTerms()
+    {
+      return map.enabled
+        ? map.GetPinCoordinates()
+          .Select(coordinates => QueryTermBuilder.BuildLocationTerm(coordinates.x, coordinates.y))
+          .ToList()
+        : new List<QueryTerm>();
+    }
+  }
+}
