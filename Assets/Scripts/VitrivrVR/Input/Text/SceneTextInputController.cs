4b41ea1ba383c8cfabb0a752776196373595d380
Florian Spiess <florian.spiess@unibas.ch>
2021-12-18 12:21:42 +0100

    Removed WindowsSpeech. #3

--- Assets/Scripts/VitrivrVR/Input/Text/SceneTextInputController.cs
+++ Assets/Scripts/VitrivrVR/Input/Text/SceneTextInputController.cs
@@ -1,6 +1,5 @@
 using Org.Vitrivr.DresApi.Model;
 using UnityEngine;
-using UnityEngine.Windows.Speech;
 using VitrivrVR.Config;
 using VitrivrVR.Submission;
 
@@ -21,22 +20,6 @@ namespace VitrivrVR.Input.Text
     {
       TextInputManager.InputBackspace();
       DresClientManager.LogInteraction("keyboard", "backspace", QueryEvent.CategoryEnum.TEXT);
-    }
-
-    /// <summary>
-    /// Adds the dictation result to the currently selected text field. Will only process dictation results with High or
-    /// Medium <see cref="ConfidenceLevel"/>.
-    /// </summary>
-    public void ReceiveWindowsSpeechDictationResult(string text, ConfidenceLevel confidence)
-    {
-      if (confidence != ConfidenceLevel.High && confidence != ConfidenceLevel.Medium) return;
-
-      InputText(text);
-      if (ConfigManager.Config.dresEnabled)
-      {
-        DresClientManager.LogInteraction("speechToText", $"input {text} {confidence.ToString()}",
-          QueryEvent.CategoryEnum.TEXT);
-      }
     }
 
     public void ReceiveDictationResult(string text)
