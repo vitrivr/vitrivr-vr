e677b2b52db75eda5f70d7248a75e35b7d2cb469
Florian Spiess <florian.spiess@unibas.ch>
2022-05-04 15:54:24 +0200

    Feature/term selection (#11)
    * Implemented basic term selection functionality.
    
    * Added extra scene to allow testing without VR.
    
    Added PreconfiguredInputScene to replace ModularInputScene for testing on conventional 2D displays.
    
    * Adjusted preconfigured term placement.
    
    * Fixed merge related issues.
    
    * Updated VR multimedia interfaces package.

---
+++ Assets/Scripts/VitrivrVR/Query/Term/QueryTermProviderFactory.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+namespace VitrivrVR.Query.Term
+{
+  /// <summary>
+  /// Instantiates query term providers.
+  /// </summary>
+  public class QueryTermProviderFactory : MonoBehaviour
+  {
+    public CombinedQueryTermProvider combinedQueryTermProvider;
+    public QueryTermProviderController queryTermProviderPrefab;
+
+    private bool _grabbed;
+    private Vector3 _offset;
+    private Quaternion _rotationOffset;
+
+    private void Start()
+    {
+      var transform1 = combinedQueryTermProvider.transform;
+      _offset = transform1.InverseTransformPoint(transform.position);
+      _rotationOffset = Quaternion.Inverse(transform1.rotation);
+    }
+
+    private void Update()
+    {
+      if (_grabbed)
+      {
+        return;
+      }
+
+      // Reset position relative to combined query term provider
+      var t = transform;
+      var otherT = combinedQueryTermProvider.transform;
+      t.position = otherT.TransformPoint(_offset);
+      t.rotation = otherT.rotation * _rotationOffset;
+    }
+
+    private void CreateQueryTermProvider()
+    {
+      var transform1 = transform;
+      var providerController = Instantiate(queryTermProviderPrefab, transform1.position, transform1.rotation);
+      combinedQueryTermProvider.queryTermProviders.Add(providerController.queryTermProvider);
+      providerController.onClose = () =>
+        combinedQueryTermProvider.queryTermProviders.Remove(providerController.queryTermProvider);
+    }
+
+    public void OnGrab()
+    {
+      _grabbed = true;
+    }
+
+    public void OnDrop()
+    {
+      _grabbed = false;
+      CreateQueryTermProvider();
+    }
+  }
+}
---
+++ Assets/Scripts/VitrivrVR/Util/CanvasCameraHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace VitrivrVR.Util
+{
+  /// <summary>
+  /// Connects a canvas with the current main camera and then destroys itself.
+  /// To be used in prefabs using a canvas to allow XR UI interaction.
+  /// </summary>
+  public class CanvasCameraHelper : MonoBehaviour
+  {
+    private void Awake()
+    {
+      GetComponent<Canvas>().worldCamera = Camera.main;
+      Destroy(this);
+    }
+  }
+}
