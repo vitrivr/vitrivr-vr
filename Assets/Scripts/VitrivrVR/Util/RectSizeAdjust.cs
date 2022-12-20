a63fcfbe0d55a3f3a69738b3b37b236715cde7cf
Florian Spiess <florian.spiess@unibas.ch>
2022-12-20 14:31:18 +0100

    Fixed Cineast instance selection in VR.

---
+++ Assets/Scripts/VitrivrVR/Util/RectSizeAdjust.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace VitrivrVR.Util
+{
+  /// <summary>
+  /// Adjusts the size of the attached object to the size of the provided rect transform on start.
+  /// </summary>
+  public class RectSizeAdjust : MonoBehaviour
+  {
+    public RectTransform adjustmentSource;
+
+    private void Start()
+    {
+      var t = transform;
+      var scale = t.localScale;
+      var sourceScale = adjustmentSource.rect;
+      scale.x = sourceScale.width;
+      scale.y = sourceScale.height;
+      t.localScale = scale;
+    }
+  }
+}
