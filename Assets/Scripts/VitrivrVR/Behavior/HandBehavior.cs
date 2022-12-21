a78fa8c182224097855168f82a3d74ea327d638c
Florian Spiess <florian.spiess@unibas.ch>
2022-12-21 17:48:28 +0100

    Added visual feedback for grip action.

---
+++ Assets/Scripts/VitrivrVR/Behavior/HandBehavior.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace VitrivrVR.Behavior
+{
+  public class HandBehavior : MonoBehaviour
+  {
+    public InputAction grip;
+
+    [Tooltip("The model to scale by the grip action.")]
+    public Transform model;
+
+    private const float ReducedScale = 0.5f;
+    private Vector3 _initialScale;
+
+    private void Start()
+    {
+      _initialScale = model.localScale;
+      grip.performed += ScaleModel;
+    }
+
+    private void OnEnable()
+    {
+      grip.Enable();
+    }
+
+    private void OnDisable()
+    {
+      grip.Disable();
+    }
+
+    private void ScaleModel(InputAction.CallbackContext context)
+    {
+      model.localScale = _initialScale * (1 - ReducedScale * context.ReadValue<float>());
+    }
+  }
+}
