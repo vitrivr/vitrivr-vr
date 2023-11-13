a1100f477852924fbb0eba236f5f8a6f5bca1a16
Florian Spiess <florian.spiess@unibas.ch>
2023-11-13 16:26:23 +0100

    Refactor Whisper dictation result events
    Exposed all Whisper prediction and result events and connected events such that dictation preview shows the result prediction and the final dictation result is correctly returned.

--- Assets/Scripts/VitrivrVR/Input/Text/WhisperDictationController.cs
+++ Assets/Scripts/VitrivrVR/Input/Text/WhisperDictationController.cs
@@ -7,95 +7,106 @@ using Whisper.Utils;
 
 namespace VitrivrVR.Input.Text
 {
-    /// <summary>
-    /// Stream transcription from microphone input.
-    /// Adapted from Whisper for Unity UPM package.
-    /// </summary>
-    public class WhisperDictationController : MonoBehaviour
+  /// <summary>
+  /// Stream transcription from microphone input.
+  /// Adapted from Whisper for Unity UPM package.
+  /// </summary>
+  public class WhisperDictationController : MonoBehaviour
+  {
+    [Serializable]
+    public class DictationStateEvent : UnityEvent<bool>
     {
-        [Serializable]
-        public class DictationStateEvent : UnityEvent<bool>
-        {
-        }
-        [Serializable]
-        public class WhisperResultEvent : UnityEvent<string>
-        {
-        }
-
-        [Serializable]
-        public class WhisperPredictionEvent : UnityEvent<string>
-        {
-        }
-        
-        public WhisperManager whisper;
-        public MicrophoneRecord microphoneRecord;
-    
-        [Header("Input")]
-        public InputAction dictateAction;
-        
-        [Header("Output")]
-        public DictationStateEvent onDictationStateChange;
-        public WhisperResultEvent onResult;
-        public WhisperPredictionEvent onPrediction;
-        
-        private WhisperStream _stream;
-
-        private async void Start()
-        {
-            _stream = await whisper.CreateStream(microphoneRecord);
-            _stream.OnResultUpdated += OnResult;
-            _stream.OnSegmentUpdated += OnSegmentUpdated;
-            _stream.OnSegmentFinished += OnSegmentFinished;
-            _stream.OnStreamFinished += OnFinished;
-
-            dictateAction.performed += OnButtonPressed;
-        }
-        
-        private void OnEnable()
-        {
-            dictateAction.Enable();
-        }
-
-        private void OnDisable()
-        {
-            dictateAction.Disable();
-        }
-
-        private void OnButtonPressed(InputAction.CallbackContext callbackContext)
-        {
-            if (!microphoneRecord.IsRecording)
-            {
-                _stream.StartStream();
-                microphoneRecord.StartRecord();
-            }
-            else
-                microphoneRecord.StopRecord();
-        
-            onDictationStateChange.Invoke(microphoneRecord.IsRecording);
-        }
-    
-    
-        private void OnResult(string result)
-        {
-            print($"Result: {result}");
-            onResult.Invoke(result);
-        }
-        
-        private void OnSegmentUpdated(WhisperResult segment)
-        {
-            print($"Segment updated: {segment.Result}");
-            onPrediction.Invoke(segment.Result);
-        }
-        
-        private void OnSegmentFinished(WhisperResult segment)
-        {
-            print($"Segment finished: {segment.Result}");
-            onPrediction.Invoke(segment.Result);
-        }
-        
-        private void OnFinished(string finalResult)
-        {
-            print($"Stream finished! {finalResult}");
-        }
     }
+
+    [Serializable]
+    public class WhisperResultEvent : UnityEvent<string>
+    {
+    }
+
+    [Serializable]
+    public class WhisperPredictionEvent : UnityEvent<string>
+    {
+    }
+
+    public WhisperManager whisper;
+    public MicrophoneRecord microphoneRecord;
+
+    [Header("Input")] public InputAction dictateAction;
+
+    [Header("Output")] public DictationStateEvent onDictationStateChange;
+
+    [Tooltip("Called when the prediction of the current segment is updated.")]
+    public WhisperPredictionEvent onSegmentPrediction;
+
+    [Tooltip("Called when the prediction of the final result is updated.")]
+    public WhisperPredictionEvent onResultPrediction;
+
+    [Tooltip("Called when the final result of the current segment is determined.")]
+    public WhisperResultEvent onSegmentResult;
+
+    [Tooltip("Called when the final result of the stream is determined.")]
+    public WhisperResultEvent onResult;
+
+
+    private WhisperStream _stream;
+
+    private async void Start()
+    {
+      _stream = await whisper.CreateStream(microphoneRecord);
+      _stream.OnResultUpdated += OnResultUpdated;
+      _stream.OnSegmentUpdated += OnSegmentUpdated;
+      _stream.OnSegmentFinished += OnSegmentFinished;
+      _stream.OnStreamFinished += OnStreamFinished;
+
+      dictateAction.performed += SetDictation;
+      dictateAction.canceled += SetDictation;
+    }
+
+    private void OnEnable()
+    {
+      dictateAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+      dictateAction.Disable();
+    }
+
+    private void SetDictation(InputAction.CallbackContext context)
+    {
+      var startDictation = context.performed;
+
+      onDictationStateChange.Invoke(startDictation);
+
+      if (startDictation)
+      {
+        _stream.StartStream();
+        microphoneRecord.StartRecord();
+      }
+      else
+      {
+        microphoneRecord.StopRecord();
+      }
+    }
+
+    private void OnResultUpdated(string result)
+    {
+      onResultPrediction.Invoke(result);
+    }
+
+    private void OnSegmentUpdated(WhisperResult segment)
+    {
+      onSegmentPrediction.Invoke(segment.Result);
+    }
+
+    private void OnSegmentFinished(WhisperResult segment)
+    {
+      onSegmentResult.Invoke(segment.Result);
+    }
+
+    private void OnStreamFinished(string finalResult)
+    {
+      onResult.Invoke(finalResult);
+    }
+  }
 }
