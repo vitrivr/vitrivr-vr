d683d7ced306dce64259e446cb59810c652bb20a
Florian Spiess <florian.spiess@unibas.ch>
2023-12-15 11:32:10 +0100

    Implement text submission field and configuration

---
+++ Assets/Scripts/VitrivrVR/Submission/TextSubmissionController.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+using VitrivrVR.Config;
+
+namespace VitrivrVR.Submission
+{
+  public class TextSubmissionController : MonoBehaviour
+  {
+    private TMP_InputField _submissionText;
+
+    private void Start()
+    {
+      _submissionText = GetComponentInChildren<TMP_InputField>();
+
+      // Check if is enabled
+      var config = ConfigManager.Config;
+      gameObject.SetActive(config.dresEnabled && config.textSubmissionEnabled);
+    }
+
+    public void SubmitText()
+    {
+      if (_submissionText.text.Length == 0) return;
+
+      DresClientManager.SubmitTextResult(_submissionText.text);
+    }
+  }
+}
