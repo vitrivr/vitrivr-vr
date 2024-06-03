6ffd0410211b689b5fd6b3ce0d6bc20a2e927800
Florian Spiess <florian.spiess@unibas.ch>
2024-06-03 16:11:21 +0200

    Implement map location display

--- Assets/Scripts/VitrivrVR/Query/Display/MinimapLocationDisplay.cs
+++ Assets/Scripts/VitrivrVR/Query/Display/MinimapLocationDisplay.cs
@@ -1,11 +1,14 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Vitrivr.UnityInterface.CineastApi.Model.Data;
 
 namespace VitrivrVR.Query.Display
 {
   public class MinimapLocationDisplay : MonoBehaviour
   {
     public ParticleSystem system;
+    public MapLocationDisplay mapLocationDisplay;
 
     private void Start()
     {
@@ -17,6 +20,7 @@ namespace VitrivrVR.Query.Display
       system.Clear();
       if (newIndex == -1)
       {
+        mapLocationDisplay.SetLocations(new List<(ScoredSegment, Vector2)>());
         return;
       }
 
@@ -53,6 +57,12 @@ namespace VitrivrVR.Query.Display
       {
         system.Emit(emitParams, 1);
       }
+
+      var scoredVectorMap = results.ToDictionary(result => result.segment.Id);
+
+      mapLocationDisplay.SetLocations(locationsResult.Points
+        .Select(point => (scoredVectorMap[point.Id], new Vector2(point.Vector[0], point.Vector[1])))
+        .ToList());
     }
   }
 }
