5d5648be958b0eb44a36121738fd5cd8c280d5f3
Florian Spiess <florian.spiess@unibas.ch>
2024-07-29 16:22:45 +0200

    Implement start of wrist display logic

---
+++ Assets/Scripts/VitrivrVR/UI/WristDisplayController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace VitrivrVR.UI
+{
+  public class WristDisplayController : MonoBehaviour
+  {
+    private GameObject _display;
+    private Transform _camera;
+
+    private void Start()
+    {
+      // Get child
+      _display = transform.GetChild(0).gameObject;
+      // Get main camera
+      if (Camera.main != null) _camera = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+      // In the update we check if the camera is looking at the wrist mounted display.
+      // If it is, we enable the display, otherwise we disable it.
+      _display.SetActive(_camera != null && Vector3.Dot(_camera.forward, transform.position - _camera.position) > .5f);
+    }
+  }
+}
